1bf06a369b6c3c65f6a7c2f4c7b5ea214f58b46a
runquanye <yer@mail.gvsu.edu>
2019-11-20 16:14:53 -0500

    update

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,21 +1,27 @@
 import React, { useContext } from "react";
 import { AppContext } from "./App";
-import { Button } from "@material-ui/core";
+import { Button, Modal } from "@material-ui/core";
 
 export const Login = props => {
     const { loggedIn, setLoggedIn } = useContext(AppContext);
 
     return (
         <div>
-            {!loggedIn && (
-                <Button
-                    onClick={() => {
-                        setLoggedIn(true);
-                    }}
-                >
-                    Login
-                </Button>
-            )}
+            <Modal
+                aria-labelledby="simple-modal-title"
+                aria-describedby="simple-modal-description"
+                open={!loggedIn}
+            >
+                <div>
+                    <Button
+                        onClick={() => {
+                            setLoggedIn(true);
+                        }}
+                    >
+                        Login
+                    </Button>
+                </div>
+            </Modal>
         </div>
     );
 };
