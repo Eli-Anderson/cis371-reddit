ed5209d93ada9befeab4ab283d21b1dd99aef6ba
Eli-Anderson <ultrasnap@yahoo.com>
2019-11-24 10:55:09 -0500

    Removed bootstrap components

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,44 +1,23 @@
 import React from "react";
 import { Feed } from "./Feed";
-import { Container, Row, Col } from "react-bootstrap";
 import { AppHeader } from "./AppHeader";
 import { useRouteMatch } from "react-router-dom";
 import { PostCreator } from "./PostCreator";
-import { Typography } from "@material-ui/core";
+import { Typography, Box } from "@material-ui/core";
 export const Home = props => {
     const match = useRouteMatch("/r/:subreddit");
     return (
-        <>
-            <Container fluid>
-                <Row>
-                    <Col>
-                        <div style={{ height: 120, width: "100%" }} id="header">
-                            <AppHeader />
-                        </div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col xs={2}></Col>
-                    <Col xs={8}>
-                        <Typography variant="h3">
-                            {(match && match.params.subreddit) || "All"}
-                        </Typography>
-                        <PostCreator
-                            subreddit={match && match.params.subreddit}
-                        />
-                        <Feed subreddit={match && match.params.subreddit} />
-                    </Col>
-                    <Col xs={2}></Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <div
-                            style={{ height: 120, width: "100%" }}
-                            id="footer"
-                        ></div>
-                    </Col>
-                </Row>
-            </Container>
-        </>
+        <Box display="flex" flexDirection="column">
+            <Box width="100%" height="100px">
+                <AppHeader />
+            </Box>
+            <Box width="100%" flexGrow="1" overflow="auto">
+                <Typography variant="h3">
+                    {(match && match.params.subreddit) || "All"}
+                </Typography>
+                <PostCreator subreddit={match && match.params.subreddit} />
+                <Feed subreddit={match && match.params.subreddit} />
+            </Box>
+        </Box>
     );
 };
