4519e3f0f109e9ace9992e929f1a7a79bfb08dca
Eli-Anderson <ultrasnap@yahoo.com>
2019-12-04 09:21:35 -0500

    Using HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { Home } from "./Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { AppAUTH } from "./db-init";
 
 const AppContext = React.createContext({
